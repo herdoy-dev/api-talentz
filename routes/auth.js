9fc8905018da439e6bcacf4c9efcf759d3332637
= <=>
2025-06-06 01:16:37 +0600

    Secure Origin

--- routes/auth.js
+++ routes/auth.js
@@ -48,8 +48,7 @@ router.post("/sign-up", async (req, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
 
@@ -78,8 +77,7 @@ router.post("/log-in", async (req, res) => {
   const token = user.generateAuthToken();
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
   return res.status(200).json({
