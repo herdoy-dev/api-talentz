88ed4d1a6a6946098f8ad3f48cdad27fb3b45754
= <=>
2025-06-20 23:54:23 -0700

    Update Webhook

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use("/api/webhook", webhook);
 
 app.use(
   cors({
@@ -55,6 +54,7 @@ app.use("/api/chats", chats);
 app.use("/api/messages", messages);
 app.use("/api/talents", talents);
 app.use("/api/orders", orders);
+app.use("/api/webhook", webhook);
 
 // Start server
 startServer(app);
